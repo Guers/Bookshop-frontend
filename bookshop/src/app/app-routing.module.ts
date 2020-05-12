99b51e3997426aa107f93e01e7f5ec93189b1946
Guers <djimafovagal@yahoo.fr>
2020-05-12 16:53:31 +0200

    Revert "pages  Connexion et bookshop"
    This reverts commit 381f319f04b6ffd6ad2fa9ac1c61e0e0cd1f0728.

--- bookshop/src/app/app-routing.module.ts
+++ bookshop/src/app/app-routing.module.ts
@@ -1,18 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { BooksComponent } from './books/books.component';
-import { BookDetailComponent } from './book-detail/book-detail.component';
-import { ConnexionComponent } from './connexion/connexion.component';
-import { InscriptionComponent } from './inscription/inscription.component';
 
 
-const routes: Routes = [
-  {path :'connexion', component :ConnexionComponent},
-  {path :'', redirectTo : '/connexion', pathMatch:'full'},
-  {path :'inscription', component :InscriptionComponent},
-  {path :'books', component :BooksComponent},
-  {path :'books/:id', component :BookDetailComponent},
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- bookshop/src/app/app.module.ts
+++ bookshop/src/app/app.module.ts
@@ -1,44 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import{ FormsModule} from '@angular/forms'
-import {CarouselModule} from 'primeng/carousel';
-import {ButtonModule} from 'primeng/button';
-import {DialogModule} from 'primeng/dialog';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {HttpClientModule} from '@angular/common/http';
-
-
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
-import { BooksComponent } from './books/books.component';
-import { BookDetailComponent } from './book-detail/book-detail.component';
-import { InscriptionComponent } from './inscription/inscription.component';
-import { ConnexionComponent } from './connexion/connexion.component';
-import { BookData } from './books/book-data';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UserComponent,
-    BooksComponent,
-    BookDetailComponent,
-    InscriptionComponent,
-    ConnexionComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    CarouselModule,
-    ButtonModule,
-    DialogModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    InMemoryWebApiModule.forRoot(BookData),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
